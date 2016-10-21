c1612130feafd7d79420f540edd2f2d3760b6ad2
darapenhchet <darapenhchet@gmail.com>
2016-10-21 15:29:08 +0900

    Updated by on

--- src/main/java/kr/ac/cbnu/bigdatalab/configurations/MvcConfiguration.java
+++ src/main/java/kr/ac/cbnu/bigdatalab/configurations/MvcConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
  * Created by PENHCHET on 10/21/2016.
+ * Updated by PENHCHET on 10/21/2016.
  */
 
 @Configuration
